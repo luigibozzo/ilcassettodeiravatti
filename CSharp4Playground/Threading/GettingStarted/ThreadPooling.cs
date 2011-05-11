954c79244fb6aaec8c97b5f98913ebe26b7de6ab
Luigi Bozzo <looeegee@gmail.com>
2011-05-11 18:09:45 +0200

    iniziato thread pooling

--- CSharp4Playground/Threading/GettingStarted/ThreadPooling.cs
+++ CSharp4Playground/Threading/GettingStarted/ThreadPooling.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Threading.GettingStarted
@@ -12,10 +14,41 @@ namespace Threading.GettingStarted
         [TestFixture]
         public class ViaTPL
         {
-            [Test]
-            public void Test1()
+            void Go()
             {
-                Assert.Pass("ok");
+                Console.WriteLine("Hello from the thread pool!");
+            }
+
+            [Test]
+            public void Create_and_start_a_new_task()
+            {
+                Task.Factory.StartNew(Go);    
+            }
+
+            
+            string DownloadString(string uri)
+            {
+                using (var wc = new System.Net.WebClient())
+                    //return wc.DownloadString(uri);
+                    return "content for uri: " + uri;
+            }
+
+            [Test]
+            public void Create_and_start_a_new_task_using_generics_for_obtaining_back_a_result()
+            {
+                // Start the task executing:
+                Task<string> task = Task.Factory.StartNew<string>
+                  (() => DownloadString("http://www.google.com"));
+
+                // We can do other work here and it will execute in parallel:
+                Go();
+                
+
+                // When we need the task's return value, we query its Result property:
+                // If it's still executing, the current thread will now block (wait)
+                // until the task finishes:
+                string result = task.Result;
+                Console.WriteLine("Got: " + result);
             }
         }
         [TestFixture]
